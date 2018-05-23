92c6a8cf480547425cb0a3f432b1b0c9a1e768a4
cnf <nanfei_130@163.com>
2018-05-23 10:49:10 +0800

    添加项目文件。

---
+++ JenkinsTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  
+        }
+    }
+}
